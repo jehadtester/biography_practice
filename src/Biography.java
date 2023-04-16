9663a6302b54d7850df517a358720678ad08b042
Jehad Mohammad <imsplashh@gmail.com>
2023-04-16 15:32:40 -0500

    added project 09

--- src/Biography.java
+++ src/Biography.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Biography {
     public static void main(String[] args) {
 
@@ -32,6 +35,66 @@ public class Biography {
          */
 
         //YOUR CODE HERE
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Book> books = new ArrayList<>(); // CREATE AN EMPTY LIST TO STORE THE BOOKS
+
+        System.out.println("What is your authors first name?");
+        String fName = input.nextLine();
+
+        System.out.println("What is your authors last name?");
+        String lName = input.nextLine();
+
+        System.out.println("Where is your author from?");
+        String country = input.nextLine();
+
+        System.out.println("Is the author still alive? (Y/N)");
+        boolean isAlive = input.nextLine().toLowerCase().equalsIgnoreCase("Y");
+        int age = 0;
+        if(isAlive){
+            System.out.println("Is your favorite author still alive? (Y/N)");
+            age = input.nextInt();
+            input.nextLine();
+        }
+
+        Author author = new Author(fName, lName, country, isAlive, age, books);
+        System.out.println("Would you like to enter book information? (Y/N)");
+        String addBook = input.nextLine();
+        while(addBook.toLowerCase().startsWith("y")){
+
+            System.out.println("What is the book name? ");
+            String title = input.nextLine();
+
+            System.out.println("What is the genre of the book ");
+            String genre = input.nextLine();
+
+            System.out.println("How many pages does the book have? ");
+            int pages = input.nextInt();
+            input.nextLine();
+
+            Book book = new Book(title, genre, pages);
+            books.add(book);
+
+            // Ask user if they want to add another book
+
+            System.out.print("Would you like to enter another book? (Y/N) ");
+            addBook = input.nextLine();
+            
+            
+
+        }
+
+        System.out.println("======= END RESULT ======");
+
+        System.out.println("Author's information = " + author);
+
+        System.out.println("Author's books are as listed below:");
+
+        for (Book book : books) {
+            System.out.println(book);
+        }
+
 
     }
 }
